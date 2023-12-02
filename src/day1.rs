309b449a760411b7bf22d1dd2304901b3e20e8cf
Quentin Balin <quentin.balin@gusto.com>
2023-12-02 08:19:33 -0500

    Move inputs in their own folder

--- src/day1.rs
+++ src/day1.rs
@@ -3,7 +3,7 @@ use std::{fs::File, io::BufReader, io::BufRead};
 const NUMBERS_AS_STRING: [char; 10] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
 pub fn run() -> std::io::Result<u32> {
-    let file = File::open("day1_input.txt").map_err(|err| panic!("File not found: {err}") ).unwrap();
+    let file = File::open("inputs/day1_input.txt").map_err(|err| panic!("File not found: {err}") ).unwrap();
     let mut reader = BufReader::new(file);
     let mut buffer = String::new();
     let mut numbers: Vec<u32> = vec![];
