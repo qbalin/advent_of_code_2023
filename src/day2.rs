f6472b8b200d50d9ebafb0d1f809f9903a40e8aa
Quentin Balin <quentin.balin@gusto.com>
2023-12-02 09:43:46 -0500

    Day 2

---
+++ src/day2.rs
@@ -0,0 +1,81 @@
+use std::{fs::File, io::BufReader, io::BufRead};
+use regex::Regex;
+
+const RED_CUBES_COUNT: u32 = 12;
+const GREEN_CUBES_COUNT: u32 = 13;
+const BLUE_CUBES_COUNT: u32 = 14;
+
+pub fn run() -> std::io::Result<u32> {
+    let file = File::open("inputs/day2_input.txt").map_err(|err| panic!("File not found: {err}") ).unwrap();
+    let mut reader = BufReader::new(file);
+    let mut buffer = String::new();
+    let mut possible_games: Vec<u32> = vec![];
+    while reader.read_line(&mut buffer)? > 0 {
+        let game_and_data: Vec<&str> = buffer.split(':').collect();
+        let game_metadata = game_and_data[0];
+        let data = game_and_data[1];
+        let sub_games: Vec<&str> = data.split(';').collect();
+        let mut all_subgames_are_possible = true;
+
+        for sub_game_data in sub_games {
+            let red_re = Regex::new(r"([0-9]+) red").unwrap();
+            for (_, [reds]) in red_re.captures_iter(sub_game_data).map(|caps| caps.extract()) {
+                let integer = reds.parse::<u32>().unwrap();
+                if integer > RED_CUBES_COUNT {
+                    all_subgames_are_possible = false;
+                    break;
+                }
+            }
+            if !all_subgames_are_possible {
+                break;
+            }
+
+            let green_re = Regex::new(r"([0-9]+) green").unwrap();
+            for (_, [greens]) in green_re.captures_iter(sub_game_data).map(|caps| caps.extract()) {
+                let integer = greens.parse::<u32>().unwrap();
+                if integer > GREEN_CUBES_COUNT {
+                    all_subgames_are_possible = false;
+                    break;
+                }
+            }
+            if !all_subgames_are_possible {
+                break;
+            }
+
+            let blue_re = Regex::new(r"([0-9]+) blue").unwrap();
+            for (_, [blues]) in blue_re.captures_iter(sub_game_data).map(|caps| caps.extract()) {
+                let integer = blues.parse::<u32>().unwrap();
+                if integer > BLUE_CUBES_COUNT {
+                    all_subgames_are_possible = false;
+                    break;
+                }
+            }
+            if !all_subgames_are_possible {
+                break;
+            }
+
+        }
+        if all_subgames_are_possible {
+            let game_number = (game_metadata.split(' ').collect::<Vec<&str>>())[1].parse::<u32>().unwrap();
+            possible_games.push(game_number);
+        }
+
+
+        buffer.clear();
+    }
+
+    let result = possible_games.iter().sum::<u32>();
+
+    Ok(result)
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn it_outputs_the_proper_result() {
+        assert_eq!(run().unwrap(), 2632);
+    }
+}
+
